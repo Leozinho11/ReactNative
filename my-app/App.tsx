d7cc5fb948840bf96a960c271e5655e8ead6d1eb
Leozinho11 <leoferraribarreto@hotmail.com>
2022-07-25 11:27:41 -0300

    25072022

---
+++ my-app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignIn } from './src/screens/SignIn'
+import { NativeBaseProvider } from 'native-base'
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+          <SignIn />
+    </NativeBaseProvider>
+  );
+}
+
